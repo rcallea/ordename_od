615089ea2d31276e576485ef36e8957d01e430e5
Ricardo <cr.calle@uniandes.edu.co>
2020-05-25 19:51:24 -0500

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from ordename_od.skeleton import fib
+
+__author__ = "Ricardo"
+__copyright__ = "Ricardo"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
